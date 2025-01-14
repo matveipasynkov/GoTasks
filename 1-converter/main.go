115bf26b2397b756168e168a073eee18c034401e
matveipasynkov <asynkovm05@icloud.com>
2025-01-14 21:09:25 +0300

    second hometask
    getData and calculate added

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -8,3 +8,22 @@ func main() {
 	eurRub := usdRub / usdEur
 	fmt.Println(eurRub)
 }
+
+func getData() (int, string, string) {
+	var (
+		value           int
+		currency_first  string
+		currency_second string
+	)
+	fmt.Print("Введите первую валюту: ")
+	fmt.Scan(&currency_first)
+	fmt.Print("Введите вторую валюту: ")
+	fmt.Scan(&currency_second)
+	fmt.Print("Значение: ")
+	fmt.Scan(&value)
+	return value, currency_first, currency_second
+}
+
+func calculate(value int, currency_first string, currency_second string) {
+
+}
