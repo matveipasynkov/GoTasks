5f44852d877b601046d185de0cb5ca9b2241de00
matveipasynkov <asynkovm05@icloud.com>
2025-01-15 22:30:28 +0300

    third hometask

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,27 +3,77 @@ package main
 import "fmt"
 
 func main() {
+	value, currencyFirst, currencySecond := getData()
+	result := calculate(value, currencyFirst, currencySecond)
+	fmt.Println("Результат в", currencySecond, "равен:", result)
+}
+
+func getData() (float64, string, string) {
+	var (
+		value          float64
+		currencyFirst  string
+		currencySecond string
+	)
+	currencyFirst = checkFirstCurrency()
+	value = checkValue()
+	currencySecond = checkSecondCurrency(currencyFirst)
+	return value, currencyFirst, currencySecond
+}
+
+func calculate(value float64, currencyFirst string, currencySecond string) float64 {
 	const usdEur = 0.98
 	const usdRub = 103.05
-	eurRub := usdRub / usdEur
-	fmt.Println(eurRub)
+	const eurRub = usdRub / usdEur
+
+	if currencyFirst == "EUR" && currencySecond == "RUB" {
+		return value * eurRub
+	} else if currencyFirst == "EUR" && currencySecond == "USD" {
+		return value / usdEur
+	} else if currencyFirst == "USD" && currencySecond == "RUB" {
+		return value * usdRub
+	} else if currencyFirst == "USD" && currencySecond == "EUR" {
+		return value * usdEur
+	} else if currencyFirst == "RUB" && currencySecond == "USD" {
+		return value / usdRub
+	} else {
+		return value / eurRub
+	}
 }
 
-func getData() (int, string, string) {
-	var (
-		value           int
-		currency_first  string
-		currency_second string
-	)
-	fmt.Print("Введите первую валюту: ")
-	fmt.Scan(&currency_first)
-	fmt.Print("Введите вторую валюту: ")
-	fmt.Scan(&currency_second)
-	fmt.Print("Значение: ")
-	fmt.Scan(&value)
-	return value, currency_first, currency_second
+func checkFirstCurrency() string {
+	var result string
+	for {
+		fmt.Print("Введите первую валюту (EUR, USD, RUB): ")
+		fmt.Scan(&result)
+		if result == "EUR" || result == "USD" || result == "RUB" {
+			return result
+		}
+		fmt.Println("Валюта введена неверно. Попробуйте ещё раз.")
+	}
 }
 
-func calculate(value int, currency_first string, currency_second string) {
+func checkSecondCurrency(firstCurrency string) string {
+	var result string
+	for {
+		fmt.Print("Введите вторую валюту (EUR, USD, RUB): ")
+		fmt.Scan(&result)
+		if result == "EUR" || result == "USD" || result == "RUB" {
+			if firstCurrency != result {
+				return result
+			}
+		}
+		fmt.Println("Валюта введена неверно, либо она повторилась. Попробуйте ещё раз.")
+	}
+}
 
+func checkValue() float64 {
+	var result float64
+	for {
+		fmt.Print("Введите значение (больше 0): ")
+		_, err := fmt.Scan(&result)
+		if err == nil && result > 0 {
+			return result
+		}
+		fmt.Println("Значение введено неверно. Попробуйте ещё раз.")
+	}
 }
